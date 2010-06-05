70900c30a720cceca76f82a5aa06a6f68b35e349
passcod <none@none>
2010-06-05 18:04:40 +1200

    book-edit: add styles

--- book-edit/index.php
+++ book-edit/index.php
@@ -1,17 +1,25 @@
-<?php
-
-
-
-?>
 <html>
 	<head>
+		<script type="text/javascript" src="http://lib/?_=jquery.js"></script>
 		<script type="text/javascript">
-
+			$(function() {
+				$('#nojs').hide();
+				$('#container').show();
+			});
 		</script>
+		<style type="text/css">
+			#container {
+				display: none;
+			}
+		</style>
 
-		<title></title>
+		<title>Editor</title>
 	</head>
 
 	<body>
+		<div id="nojs">You need javascript.</div>
+		<div id="container">
+			<p>TODO</p>
+		</div>
 	</body>
 </html>
