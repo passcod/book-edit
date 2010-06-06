2ab9e0b1770c3e2e5f7e84e554c5cccee18a57f9
passcod <none@none>
2010-06-06 20:19:07 +1200

    +: yWN date, line >80 limit. -: hidden content for no js

--- book-edit/index.php
+++ book-edit/index.php
@@ -1,10 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 	<head>
 		<script type="text/javascript" src="http://lib/?_=jquery.js"></script>
 		<script type="text/javascript">
 			$(function() {
-				$('#nojs').hide();
-				$('#menu').show();
+				$('#editor').hide();
 				
 				$('#refresh').click(function() {
 					$.get('thebook/refresh.php', function(d) {
@@ -24,6 +25,7 @@
 				
 				$('#back').click(function() {
 					$('#editor').hide();
+					$('#refstat').text('Ready');
 					$('#menu').show();
 				});
 				
@@ -39,6 +41,7 @@
 					$('#edittext').height( $(window).height() * 0.8 ).val(data);
 					$('#edittext').keyup(viewMe);
 					$('#editview').html( mark.makeHtml(data) );
+					$('#status').text('Ready');
 					$('#editor').show();
 				});
 			}
@@ -57,14 +60,32 @@
 				var mark = new Showdown.converter();
 				var md = $('#edittext').val();
 				$('#editview').html( mark.makeHtml(md) );
+				checkLines();
+			}
+			
+			function checkLines() {
+				var md = $('#edittext').val(); 
+				var lines = md.split("\n");
+				var over80 = '';
+				
+				i = 0;
+				for( i in lines ) {
+					if ( lines[i].length > 79 ) {
+						over80 += i+', ';
+					}
+				}
+				
+				if ( over80 != '' ) {
+					$('#lines').text('These lines are over 80 chars: '+over80);
+				}
+				else
+				{
+					$('#lines').text('');
+				}
 			}
 		</script>
 		<style type="text/css">
 			@import url(thebook/page.css);
-		
-			#menu, #editor {
-				display: none;
-			}
 			
 			#filelist {
 				list-style-type: none;
@@ -96,6 +117,16 @@
 				-moz-border-radius: 5px;
 				-webkit-border-radius: 5px;
 				padding: 5px;
+				float: right;
+			}
+			
+			#date {
+				text-align: center;
+			}
+			
+			#lines {
+				font-size: small;
+				color: #b00;
 			}
 		</style>
 
@@ -104,7 +135,7 @@
 
 	<body>
 		<div id="container">
-			<div id="nojs">You need javascript.</div>
+			<div id="date"><?php echo date('y.W.N'); ?></div>
 			<div id="menu">
 				<p>Files:</p>
 				<ul id="filelist"></ul>
@@ -112,10 +143,11 @@
 				<p id="refstat">Ready</p>
 			</div>
 			<div id="editor">
-				<p><span id="status">Ready</span><a id="back" style="float: right;">Back</a></p>
+				<p><span id="status">Ready</span>
+				<a id="back" style="float: right;">Back</a></p>
 				<textarea id="edittext"></textarea>
 				<br />
-				<p><button id="save">Save</button></p>
+				<p><span id="lines"></span><button id="save">Save</button></p>
 				<hr />
 				<br />
 				<div id="editview"></div>
